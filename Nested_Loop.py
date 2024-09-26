66e5fd8a053d7da3572e511c9833e595b1db78d4
15608 <zachary.geraghty@vtsd.com>
2024-09-26 13:55:53 -0400

    sTEP 20

---
+++ Nested_Loop.py
@@ -0,0 +1,20 @@
+'''
+
+
+
+#6 times five
+num = 0
+product = 6
+for num in range(5):
+    product+= 6
+'''
+
+num1 = 0
+num2 = 0
+total = 0
+
+for num1 in range(5):
+    for num2 in range(5):
+        total+=1
+
+print(total)
